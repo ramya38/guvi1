50085ba084bf5ff74a2ee287cc0f06f93c4ee69c
ramya38 <sramya2309@gmail.com>
2017-06-09 15:08:55 +0530

    Create abc.java

---
+++ abc.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class abc
+{
+	public static void main(String args[])
+	{
+		int n;
+		Scanner sc=new Scanner(System.in);
+		n=sc.nextInt();
+		if(n==0)
+		System.out.println(n+"is zero");
+		else if(n<0)
+		System.out.println(n+"is negative");
+		else
+		System.out.println(n+"is positive");
+	}
+}
+	
